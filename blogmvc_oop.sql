c085c491e2bcb64e67dfd2cc81e084ffe04a3fca
Roaa Alhakimi <58307502+ro5a@users.noreply.github.com>
2022-03-20 08:49:35 +0300

    edit database

--- blogmvc_oop.sql
+++ blogmvc_oop.sql
@@ -1,45 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 20 مارس 2022 الساعة 06:48
+-- إصدار الخادم: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: 'blogmvc_oop'
+-- Database: `blogmvc_oop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table 'posts'
+-- بنية الجدول `posts`
 --
 
-CREATE TABLE IF NOT EXISTS posts (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  title varchar(255) DEFAULT NULL,
-  small_desc text NOT NULL,
-  content longtext NOT NULL,
-  author varchar(255) NOT NULL,
-  date timestamp NOT NULL DEFAULT NOW(),
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS users (
-  id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
-  password char(255) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+CREATE TABLE `posts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `small_desc` text NOT NULL,
+  `content` longtext NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `users`
+-- إرجاع أو استيراد بيانات الجدول `posts`
 --
 
-INSERT INTO users (username, password) VALUES
-('admin', '$2y$10$Gxsl/dZSgxKgdMmJjVDpGufd9RJPAi13YbVV.cesdHhtcoOkCOJ8y');
+INSERT INTO `posts` (`id`, `title`, `small_desc`, `content`, `author`, `date`) VALUES
+(1, 'post1', 'hello', 'hello there', 'roaa', '2022-03-18 21:00:00'),
+(2, 'post2', 'hello ', 'there is for read this post', 'Roaa', '2022-03-19 21:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- بنية الجدول `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` char(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- إرجاع أو استيراد بيانات الجدول `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$Gxsl/dZSgxKgdMmJjVDpGufd9RJPAi13YbVV.cesdHhtcoOkCOJ8y'),
+(2, 'admin1', '123456789');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
